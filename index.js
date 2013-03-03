942c218967dca91b6372f96d867ffdc99d62b1b9
Daniel Friedman <danielnfriedman@gmail.com>
2013-03-02 21:06:59 -0800

    parse nmap xml file and send it to ninja cloud

--- index.js
+++ index.js
@@ -1,51 +1,102 @@
-var Device = require('./lib/device')
+var TotalCount = require('./lib/totalCount.js')
+  , PortCount = require('./lib/portCount.js')
   , util = require('util')
-  , stream = require('stream');
+  , stream = require('stream')
+  , xml = require('libxml-to-js')
+  , fs = require('fs')
+  , exec = require('child_process').exec;
 
 // Give our module a stream interface
-util.inherits(myModule,stream);
+util.inherits(nmap,stream);
 
-/**
- * Called when our client starts up
- * @constructor
- *
- * @param  {Object} opts Saved/default module configuration
- * @param  {Object} app  The app event emitter
- * @param  {String} app.id The client serial number
- *
- * @property  {Function} save When called will save the contents of `opts`
- * @property  {Function} config Will be called when config data is received from the cloud
- *
- * @fires register - Emit this when you wish to register a device (see Device)
- * @fires config - Emit this when you wish to send config data back to the cloud
- */
-function myModule(opts,app) {
+// 1. Run nmap on a loop
+// 2. Analyse after nmap has run
+// 3. Report Device
+//
+// Devices:
+// 1) Total Server Count G: 0
+// 2) Server count per port G: 80/3000/etc
+//
 
+var DEBUG = true;
+
+function nmap(opts,app) {
+  this.app = app;
+  app.on('client::up',this.parseXML.bind(this));
+
+
+};
+
+
+
+nmap.prototype.parseXML = function() {
   var self = this;
+  var xmlStr = fs.readFileSync(__dirname+'/results.xml').toString();
 
-  app.on('client::up',function(){
+  if (xmlStr.indexOf('</nmaprun>')===-1) xmlStr += '</nmaprun>';
 
-    // The client is now connected to the cloud
+  xml(xmlStr,function(err,data) {
 
-    // Do stuff with opts, and then commit it to disk
-    if (!opts.hasMutated) {
-      opts.hasMutated = true;
+    if (err) return;
+    if (!data.host) return;
+
+    var totalCount = new TotalCount();
+    if (!DEBUG) this.emit('register', totalCount);
+
+    totalCount.emit('data',data.host.length);
+
+    var ports = {};
+    // Iterate over all the ports, counting them up
+    for (var i=0;i<data.host.length;i++) {
+
+      var portObj = data.host[i].ports.port;
+
+      if (util.isArray(portObj)) {
+
+        for (var j=0;j<portObj.length;j++) {
+
+          var port = portObj[j]['@'].portid
+          ports[port] = ports[port] || 0;
+          ports[port]++;
+
+        }
+
+      } else {
+
+        var port = portObj['@'].portid;
+        ports[port] = ports[port] || 0;
+        ports[port]++;
+
+      }
     }
 
-    self.save();
+    for (var port in ports) {
 
-    // Register a device
-    self.emit('register', new Device());
-  });
+      var device = self.fetchPortDevice(port);
+      device.emit('data',ports[port]);
+
+    }
+  }.bind(this));
+
 };
 
+var devices={};
 /**
- * Called when config data is received from the cloud
- * @param  {Object} config Configuration data
+ * Create and return the port device, and register it
+ * if it does not already exist
+ * @param  {Number} port The port number
+ * @return {Object} instanceof PortCount
  */
-myModule.prototype.config = function(config) {
+nmap.prototype.fetchPortDevice = function(port) {
 
-};
+  if (!devices.hasOwnProperty(port)) {
+
+    var device = new PortCount(port);
+    devices[port] = device;
+    if (!DEBUG) this.emit('register', device);
+  }
+  return devices[port];
+}
 
 // Export it
-module.exports = myModule;
+module.exports = nmap;
